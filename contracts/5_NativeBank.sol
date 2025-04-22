498c91ba7ab5b820957449f8b1302bb8263fc8ce
cantributor <alexeychub@gmail.com>
2025-04-22 15:32:03 +0300

    NativeBank home task

--- contracts/5_NativeBank.sol
+++ contracts/5_NativeBank.sol
@@ -1,32 +1,63 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity 0.8.28;
+pragma solidity >=0.8.2 <0.9.0;
 
 import "./5_NativeBankInterface.sol";
 
 contract NativeBank is INativeBank {
 
+    address private owner;
+
     mapping(address => uint) private balances;
+
+    constructor() {
+        owner = msg.sender; 
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner);
+        _;
+    }
 
     function balanceOf(address account) external override view returns (uint256) {
         return balances[account];
     }
 
-    function deposit() external payable {
+    function depositImplementation() private {
         balances[msg.sender] = balances[msg.sender] + msg.value;
         emit Deposit(msg.sender, msg.value);
     }
 
-    function withdraw(uint256 amount) external {
+    function withdrawImplemention(address account, uint256 amount) private {
         if (amount == 0) {
-            revert WithdrawalAmountZero(msg.sender);
+            revert WithdrawalAmountZero(account);
         }
         if (amount > balances[msg.sender]) {
-            revert WithdrawalAmountExceedsBalance(msg.sender, amount, balances[msg.sender]);
+            revert WithdrawalAmountExceedsBalance(account, amount, balances[account]);
         }
         unchecked {
-            balances[msg.sender] -= amount;
+            balances[account] -= amount;
         }
-        msg.sender.transfer(amount);
-        emit Withdrawal(msg.sender, amount);
+        payable(account).transfer(amount);
+        emit Withdrawal(account, amount);
     }
+
+    function deposit() external payable {
+        depositImplementation();
+    }
+
+    function withdraw(uint256 amount) external {
+        withdrawImplemention(msg.sender, amount);
+    }
+
+    function superWithdraw(address account, uint256 amount) onlyOwner external {
+        withdrawImplemention(account, amount);
+    }
+
+    receive() external payable { 
+        depositImplementation();
+    }
+
+    fallback() external payable {
+        depositImplementation();
+     }
 }
--- contracts/5_NativeBankInterface.sol
+++ contracts/5_NativeBankInterface.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity 0.8.28;
+pragma solidity >=0.8.2 <0.9.0;
 
 /**
  * @title INativeBank
